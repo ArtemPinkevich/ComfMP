f84f9544e916e2a7c145e796972a63b6bd38a74b
Artem Pinkevich <Artem.Pinkevich@gmail.com>
2014-01-04 21:13:39 +0700

    Added files for project. Demo.

---
+++ trank/src/com/example/ComfMP/Plaer.java
@@ -0,0 +1,11 @@
+package com.example.ComfMP;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Артём
+ * Date: 04.01.14
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Plaer {
+}
